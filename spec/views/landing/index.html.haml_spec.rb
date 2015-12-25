cf49cc2dade46ee337c5970c70d88f4dfe2c8ea3
d-theus <slma0x02@gmail.com>
2015-12-25 12:23:20 +0300

    landing: specs

--- spec/views/landing/index.html.haml_spec.rb
+++ spec/views/landing/index.html.haml_spec.rb
@@ -3,7 +3,7 @@ require 'rails_helper'
 RSpec.describe "landing/index.html.haml", type: :view do
   before :each do render end
   it 'has title' do
-    expect(rendered).to have_selector 'title'
+    expect(rendered).to have_selector('title', text: /^HomeMade/)
   end
   it 'has meta-description' do
     expect(rendered).to have_selector 'meta[name="description"]'
@@ -11,11 +11,13 @@ RSpec.describe "landing/index.html.haml", type: :view do
   it 'has meta-keywords' do
     expect(rendered).to have_selector 'meta[name="keywords"]'
   end
-
+  it 'has phone' do
+    expect(rendered).to have_link /.*/, href: /^tel:/
+  end
   it 'has offer link' do
-    expect(rendered).to have_selector "a[href=#{offer_path}]"
+    expect(rendered).to have_link 'Оферта', href: offer_path
   end
   it 'has policy link' do
-    expect(rendered).to have_selector "a[href=#{policy_path}]"
+    expect(rendered).to have_link 'Конфиденциальность', href: policy_path
   end
 end
