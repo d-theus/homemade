09ee4c450e329bce3843f7ba4b250457ce6b9f0b
d-theus <slma0x02@gmail.com>
2015-12-28 16:16:45 +0300

    menu mockup

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+  SRCW = { 'lg' => 1440, 'md' => 920, 'sm' => 520}
+  def card(&block)
+    capture_haml do
+      haml_tag :div, class: 'card' do
+        yield
+      end
+    end
+  end
+
+  def card_image(image, &block)
+    capture_haml do
+      haml_tag :div, class: 'card-img-container' do
+        haml_tag :img, src: image_path(image)
+      end
+    end
+  end
 end
