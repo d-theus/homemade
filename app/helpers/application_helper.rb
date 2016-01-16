71c041ae82f6f08855307c30306b2c2b82e0d631
d-theus <slma0x02@gmail.com>
2016-01-16 10:05:45 +0300

    date and phone helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,4 +19,40 @@ module ApplicationHelper
   def admin?
     admin_signed_in?
   end
+
+  def date_format(date)
+    I18n.l(date, format: '%e %B')
+  end
+
+  def next_week_begin(today = Date.today)
+    if today.days_to_week_start < 4
+      date_format(today.next_week)
+    else
+      date_format(today.next_week + 1.week)
+    end
+  end
+
+  def next_week_end(today = Date.today)
+    if today.days_to_week_start < 4
+      date_format(today.next_week + 4.days)
+    else
+      date_format(today.next_week + 11.days)
+    end
+  end
+
+  def next_sunday(today = Date.today)
+    if today.days_to_week_start < 4
+      date_format(today.sunday)
+    else
+      date_format(today.next_week.sunday)
+    end
+  end
+
+  def phone_number(number)
+    country = number[0..1]
+    code = number[2..4]
+    num1  = number[5..7]
+    num2  = number[8..11]
+    "#{country} (#{code}) #{num1} #{num2}"
+  end
 end
--- app/helpers/landing_helper.rb
+++ app/helpers/landing_helper.rb
@@ -2,35 +2,4 @@ module LandingHelper
   def landing?
     controller_name == 'landing'
   end
-
-  def date_format(date)
-    I18n.l(date, format: '%e %B')
-  end
-
-  def next_week_begin
-    today = Date.today
-    if today.days_to_week_start < 4
-      date_format(today.next_week)
-    else
-      date_format(today.next_week + 1.week)
-    end
-  end
-
-  def next_week_end
-    today = Date.today
-    if today.days_to_week_start < 4
-      date_format(today.next_week + 4.days)
-    else
-      date_format(today.next_week + 11.days)
-    end
-  end
-
-  def next_sunday
-    today = Date.today
-    if today.days_to_week_start < 4
-      date_format(today.sunday)
-    else
-      date_format(today.next_week.sunday)
-    end
-  end
 end
