f3b8648033771d3b32c2708dc52bf0819d0d3009
d-theus <slma0x02@gmail.com>
2016-01-19 16:40:46 +0300

    order modal

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -2,5 +2,6 @@ class LandingController < ApplicationController
   def index
     @recipes = Recipe.featured.includes(:inventory_items)
     @subscription = WeeklyMenuSubscription.new
+    @order = Order.new
   end
 end
