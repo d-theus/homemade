e71fb38982760cf7de7cf9ba258f14a08345a31a
d-theus <slma0x02@gmail.com>
2016-01-20 17:36:46 +0300

    recipe#new&edit collection for day

--- app/helpers/recipes_helper.rb
+++ app/helpers/recipes_helper.rb
@@ -2,4 +2,11 @@ module RecipesHelper
   def human_day(day)
     %w(первый второй третий четвёртый пятый)[day - 1]
   end
+
+  def options_for_day
+    result = []
+    result << ['Нет', nil]
+    [*1..5].each { |i| result << [i,i] }
+    result
+  end
 end
