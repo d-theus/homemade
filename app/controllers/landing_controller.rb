664a6cef41e5d60d9953bd95382c7c3f9b70c3e5
d-theus <slma0x02@gmail.com>
2016-03-28 14:34:39 +0300

    handle paid order

--- app/controllers/landing_controller.rb
+++ app/controllers/landing_controller.rb
@@ -1,12 +1,20 @@
 class LandingController < ApplicationController
+  before_action :handle_unpaid_order, only: [:index]
   def index
     @recipes = Recipe.featured.order('DAY ASC').includes(:inventory_items)
     @subscription = WeeklyMenuSubscription.new
     @order = Order.new
+  end
+
+  private
+
+  def handle_unpaid_order
     if cookies[:last_order]
       order = Order.find(cookies[:last_order])
       if order.try(:can_pay?)
         @unpaid_order = order
+      else
+        cookies.delete(:last_order)
       end
     end
   end
